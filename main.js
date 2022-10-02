1dd8a744d8399b824df500c1e7bea824ed6db6a6
Smail <44433524+Smail@users.noreply.github.com>
2022-10-02 17:57:20 +0200

    Use requestAnimationFrame

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-const mandelbrotCanvas = document.getElementById("mandelbrot-canvas");
-
 function setCanvasSize(canvas) {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 }
 
-setCanvasSize(mandelbrotCanvas);
+function drawFrame() {
+  const mandelbrotCanvas = document.getElementById("mandelbrot-canvas");
+  setCanvasSize(mandelbrotCanvas);
+}
 
-const ctx = mandelbrotCanvas.getContext("2d");
+requestAnimationFrame(drawFrame);
