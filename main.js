6a3df8c38008c9d71c038f19a8d172fb76c0f325
Smail <44433524+Smail@users.noreply.github.com>
2022-10-02 18:38:37 +0200

    Perf: Use GPU

--- main.js
+++ main.js
@@ -3,41 +3,47 @@ function setCanvasSize(canvas) {
   canvas.height = window.innerHeight;
 }
 
-function drawMandelbrot(canvas, maxIter) {
-  const ctx = canvas.getContext("2d");
-
-  for (let px = 0; px < canvas.width; px++) {
-    for (let py = 0; py < canvas.height; py++) {
-      const x0 = (px / canvas.width) * (2.47) - 2;
-      const y0 = (py / canvas.height) * (2.47) - 1.12;
-      let x = x0;
-      let y = y0;
-
-      for (let t = 0; t < maxIter; t++) {
-        const xTemp = (x * x) - (y * y) + x0;
-        y = 2 * x * y + y0;
-        x = xTemp;
-
-        if (xTemp > maxIter) {
-          const tNormal = t / maxIter;
-          const red = tNormal;
-          const green = tNormal;
-          const blue = tNormal;
-
-          ctx.fillStyle = `rgb(${red * 3}, ${green}, ${blue / 2})`;
-          ctx.fillRect(px, py, 3, 3);
-
-          break;
-        }
-      }
-    }
-  }
-}
-
 function drawFrame() {
   const mandelbrotCanvas = document.getElementById("mandelbrot-canvas");
-  setCanvasSize(mandelbrotCanvas);
-  drawMandelbrot(mandelbrotCanvas, 200);
+
+  render.setOutput([window.innerWidth, window.innerHeight]);
+  render(window.innerWidth, window.innerHeight);
+
+  mandelbrotCanvas.replaceWith(render.canvas);
+  render.canvas.id = "mandelbrot-canvas";
 }
+
+const gpu = new GPU();
+const render = gpu.createKernel(function (width, height) {
+  const maxIter = this.constants.iterations;
+  const px = this.thread.x;
+  const py = this.thread.y;
+
+  this.color(1, 1, 1, 1);
+
+  const x0 = (px / width) * (2.47) - 2;
+  const y0 = (py / height) * (2.47) - 1.12;
+  let x = 0.0;
+  let y = 0.0;
+
+  for (let i = 0; i < maxIter; i++) {
+    const xTemp = (x * x) - (y * y) + x0;
+    y = 2 * x * y + y0;
+    x = xTemp;
+
+    if (xTemp > maxIter) {
+      const tNormal = i / maxIter;
+      const red = tNormal * (Math.cos(i) + 1) / 2;
+      const green = tNormal;
+      const blue = tNormal * 0.5;
+
+      this.color(red, green, blue, 1);
+      break;
+    }
+  }
+}, {
+  constants: {iterations: 1000}
+}).setDynamicOutput(true)
+  .setGraphical(true);
 
 requestAnimationFrame(drawFrame);
